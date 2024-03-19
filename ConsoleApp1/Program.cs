702605a9c7c4aa247597aed173f6bf61ef9d5231
matvey.nizamutdinov <matvey.nizamutdinov@DESKTOP-ODO1JRT>
2024-03-19 13:37:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    static extern bool SetComputerName(string computerName);
+
+    static void Main(string[] args)
+    {
+        // Генерация рандомных цифр
+        Random rand = new Random();
+        int randomNumber = rand.Next(1000, 9999); // Генерируем 4-х значное число
+
+        // Формируем новое имя компьютера
+        string newComputerName = "WORK" + randomNumber;
+
+        // Меняем имя компьютера
+        if (SetComputerName(newComputerName))
+        {
+            Console.WriteLine("Имя компьютера успешно изменено на: " + newComputerName);
+        }
+        else
+        {
+            Console.WriteLine("Ошибка при изменении имени компьютера.");
+        }
+
+        Console.ReadLine();
+    }
+}
